f239b634ee2d7a228b8aae7e33c21baccdd3ef55
Ramyasri135 <kurvaramyasri4242@gmail.com>
2025-01-25 14:50:46 +0530

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Reverse{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a string");
+        String str=sc.nextLine();
+        String str1="";
+        for(int i=str.length()-1;i>=0;i--){
+            str1=str1 +str.charAt(i);
+        }
+        sc.close();
+        System.out.println("Reversed string is:"+str1);
+       
+    }
+   
+}
